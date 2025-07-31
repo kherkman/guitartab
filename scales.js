cc9102869a13f3a19c78515d3066c5009f477b8a
kherkman <kristian.herkman@gmail.com>
2025-07-31 14:54:50 +0300

    Add files via upload

---
+++ scales.js
@@ -0,0 +1,54 @@
+const SCALE_DEFINITIONS = {
+    'Major (Ionian)': [2, 2, 1, 2, 2, 2, 1],
+    'Dorian': [2, 1, 2, 2, 2, 1, 2],
+    'Phrygian': [1, 2, 2, 2, 1, 2, 2],
+    'Lydian': [2, 2, 2, 1, 2, 2, 1],
+    'Mixolydian': [2, 2, 1, 2, 2, 1, 2],
+    'Minor (Aeolian)': [2, 1, 2, 2, 1, 2, 2],
+    'Locrian': [1, 2, 2, 1, 2, 2, 2],
+    'Harmonic Minor': [2, 1, 2, 2, 1, 3, 1],
+    'Locrian #6': [1, 2, 2, 1, 3, 1, 2],
+    'Phrygian Dominant': [1, 3, 1, 2, 1, 2, 2],
+    'Melodic Minor': [2, 1, 2, 2, 2, 2, 1],
+    'Major Pentatonic': [2, 2, 3, 2, 3],
+    'Minor Pentatonic': [3, 2, 2, 3, 2],
+    'Blues': [3, 2, 1, 1, 3, 2],
+    'Neapolitan Minor': [1, 2, 2, 2, 1, 3, 1],
+    'Byzantine': [1, 3, 1, 2, 1, 3, 1]
+};
+
+const SCALE_DEFINITIONS_19 = {
+    'Supermajor Lydian (19)': [3, 4, 1, 3, 3, 3, 2],
+    'Subminor Pentatonic (19)': [4, 2, 5, 4, 4]
+};
+
+const SCALE_DEFINITIONS_24 = {
+    'Neutral Ionian (24)': [4, 3, 3, 4, 4, 3, 3],
+    'Neutral Dorian (24)': [3, 3, 4, 4, 3, 3, 4],
+    'Neutral Phrygian (24)': [3, 4, 4, 3, 3, 4, 3],
+    'Neutral Lydian (24)': [4, 4, 3, 3, 4, 3, 3],
+    'Neutral Mixolydian (24)': [4, 3, 3, 4, 3, 3, 4],
+    'Neutral Aeolian (24)': [3, 3, 4, 3, 3, 4, 4],
+    'Neutral Locrian (24)': [3, 4, 3, 3, 4, 4, 3],
+    'Neutral Whole (24)': [3, 4, 3, 4, 3, 4, 3],
+    'Maqam Bayati (24)': [3, 2, 4, 4, 2, 4, 4],
+    'Maqam Rast Asc (24)': [4, 3, 2, 4, 4, 3, 4],
+    'Maqam Rast Desc (24)': [4, 3, 2, 4, 4, 4, 3],
+    'Maqam Saba (24)': [3, 2, 2, 6, 2, 4, 4],
+    'Maqam Sigah (24)': [2, 4, 4, 3, 2, 4, 5],
+    'Maqam Ajam (24)': [4, 4, 2, 4, 4, 4, 2],
+    'Maqam Hoseyni (24)': [3, 2, 4, 3, 2, 4, 6],
+    'Maqam Nahawand (24)': [4, 2, 4, 4, 2, 4, 4],
+    'Maqam Hijaz (24)': [2, 6, 2, 4, 2, 4, 4],
+    'Maqam Kurd (24)': [2, 4, 4, 2, 4, 4, 4]
+};
+
+const SCALE_DEFINITIONS_31 = {
+    'Neutral (31)': [4, 4, 3, 4, 4, 4, 4],
+    'Gamelan Pelog (31)': [3, 4, 7, 3, 4, 7, 3],
+    'Gamelan pseudo-Slendro (31)': [6, 6, 6, 6, 7],
+    'Pure Blues (31)': [10, 8, 7, 6],
+    'Undecimal Blues (31)': [10, 4, 4, 9, 4],
+    'Mothra (Square Wave) (31)': [5, 5, 4, 4, 4, 3, 3, 3],
+    'Supermajor Hexatonic (31)': [5, 6, 2, 5, 7, 6]
+};
